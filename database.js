7b4835f8189958bd9c05a339910670979c68a441
Mjolksaft <Kalla01@hotmail.se>
2024-02-13 17:44:14 +0100

    hard coded device names for now

--- database.js
+++ database.js
@@ -25,15 +25,15 @@ async function saveData(data) {
         const { led, fan, window, door, motion, light, gas } = data;
 
         // Update device data in the Device model
-        await deviceModel.findOneAndUpdate({ name: 'LED' }, { status: led });
-        await deviceModel.findOneAndUpdate({ name: 'Fan' }, { status: fan });
-        await deviceModel.findOneAndUpdate({ name: 'Window' }, { status: window });
-        await deviceModel.findOneAndUpdate({ name: 'Door' }, { status: door });
-        await deviceModel.findOneAndUpdate({ name: 'Motion' }, { status: motion });
+        await deviceModel.findOneAndUpdate({ name: 'led' }, { status: led });
+        await deviceModel.findOneAndUpdate({ name: 'fan' }, { status: fan });
+        await deviceModel.findOneAndUpdate({ name: 'window' }, { status: window });
+        await deviceModel.findOneAndUpdate({ name: 'door' }, { status: door });
+        await deviceModel.findOneAndUpdate({ name: 'motion' }, { status: motion });
 
         // Update sensor data in the Sensor model
-        await sensorModel.findOneAndUpdate({ name: 'Light' }, { value: light });
-        await sensorModel.findOneAndUpdate({ name: 'Gas' }, { value: gas });
+        await sensorModel.findOneAndUpdate({ name: 'light' }, { value: light });
+        await sensorModel.findOneAndUpdate({ name: 'gas' }, { value: gas });
 
         console.log('Arduino data updated in the database.');
     } catch (error) {
